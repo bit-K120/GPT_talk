86c1ad90bc38a269371452b7f1b612968bd018c0
bit-K120 <kayugen105@gmail.com>
2023-11-04 16:08:16 +0900

    chat flow reversed

--- flask_test.py
+++ flask_test.py
@@ -1,17 +1,13 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO  
 
 
 app = Flask(__name__)
-# socketio = SocketIO(app)
 
 
-@app.route('/chat', methods = ["GET"])
+@app.route("/", methods=["GET"])
 def chat():
-    try:
-        return render_template("chat_1.html")
-    except Exception as e:
-        return str(e)
+    return render_template("chat_1.html")
 
-if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=8000)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="127.0.0.1", port=8000)
